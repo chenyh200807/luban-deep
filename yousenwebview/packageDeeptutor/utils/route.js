6da302db02b651a4c858696fa4f925e669dcbe91
Yehong Chen <cainkyking@gmail.com>
2026-07-05 12:53:21 +0800

    feat(miniprogram): route 注册 luban/concept-cards 页路径

--- yousenwebview/packageDeeptutor/utils/route.js
+++ yousenwebview/packageDeeptutor/utils/route.js
@@ -9,6 +9,7 @@ var KNOWN_PACKAGE_PATHS = {
   "pages/login/manual": true,
   "pages/login/reset-password": true,
   "pages/learn/learn": true,
+  "pages/luban/concept-cards/concept-cards": true,
   "pages/luban/review/review": true,
   "pages/luban/stations/stations": true,
   "pages/mistake-book/mistake-book": true,
@@ -110,6 +111,9 @@ module.exports = {
   },
   lubanReview: function (query) {
     return withQuery("pages/luban/review/review", query);
+  },
+  lubanConceptCards: function (query) {
+    return withQuery("pages/luban/concept-cards/concept-cards", query);
   },
   profile: function () {
     return resolve("pages/profile/profile");
